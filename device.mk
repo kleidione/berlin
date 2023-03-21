65de4e9b23d607edd4f804c0a3daef91c8eca7fd
kleidione Freitas <kleidione@gmail.com>
2023-03-21 08:08:56 -0300

    berlin: Rebranded LineageSystemUI
    Signed-off-by: kleidione <kleidione@gmail.com>

--- device.mk
+++ device.mk
@@ -37,7 +37,7 @@ $(call inherit-product, device/motorola/sm7325-common/common.mk)
 # Overlay
 PRODUCT_PACKAGES += \
     FrameworksResBerlin \
-    LineageSystemUIBerlin \
+    CustomSystemUIBerlin \
     SettingsProviderResBerlin \
     SystemUIResBerlin \
     NfcResBerlin \
