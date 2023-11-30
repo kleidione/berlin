9a57fa96672b5fcee4e42fef3b3fdb5c8fafa3cd
kleidione Freitas <kleidione@gmail.com>
2023-11-29 22:53:03 -0300

    berlin: Update to T1RGS33.135-109-9-5
    Signed-off-by: kleidione <kleidione@gmail.com>

--- aosp_berlin.mk
+++ aosp_berlin.mk
@@ -38,6 +38,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=berlin_retail \
-    PRIVATE_BUILD_DESC="berlin_global-user 13 T1RGS33.135-109-9-3 b91f3 release-keys"
+    PRIVATE_BUILD_DESC="berlin_global-user 13 T1RGS33.135-109-9-5 76d71b release-keys"
 
-BUILD_FINGERPRINT := motorola/berlin_global/berlin:13/T1RGS33.135-109-9-3/b91f3:user/release-keys
+BUILD_FINGERPRINT := motorola/berlin_global/berlin:13/T1RGS33.135-109-9-5/76d71b:user/release-keys
